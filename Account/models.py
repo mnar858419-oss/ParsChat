da705878e68447c375595dbeaf00776b5469ef3c
Mohammad Nasiri <Mohammad_Nasiri@gmail.com>
2025-10-03 11:10:35 +0330

    start database

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class CustomUser(AbstractUser):
+     bio = models.TextField(
+        max_length=500,
+        null=True,
+        blank=True,
+        verbose_name='بیوگرافی' 
+        )
+     profile_picture = models.ImageField(
+        upload_to='profile_pics/',
+        blank=True,
+        null=True,
+        verbose_name='عکس پروفایل'
+        )
+     class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+        
+     def __str__(self):
+        return self.username
+     
--- Messaging/models.py
+++ Messaging/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Conversation(models.Model):
+     participants = models.ManyToManyField(
+        User,
+        related_name='Conversations',
+        verbose_name='شرکت‌کنندگان'
+    )
+     last_message_at = models.DateTimeField(
+        default=timezone.now,
+        verbose_name='آخرین زمان فعالیت'
+    )
+     class Meta:
+        verbose_name = 'مکالمه'
+        verbose_name_plural = 'مکالمات'
+        ordering = ['-last_message_at']
+
+     def __str__(self):
+        return f"Conversation ({self.participants.count()} participants)"
+
+class Message(models.Model):
+
+     Conversation = models.ForeignKey(
+        Conversation,
+        on_delete=models.CASCADE,
+        related_name='messages',
+        verbose_name='مکالمه'
+     )
+
+     sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='sent_messages',
+        verbose_name='فرستنده'
+     )
+
+     content = models.TextField(
+        verbose_name='متن پیام'
+     )
+
+     timestamp = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='زمان ارسال'
+     )
+
+     is_read = models.BooleanField(
+        default=False,
+        verbose_name='خوانده شده'
+     )
+
+     class Meta:
+        verbose_name = 'پیام'
+        verbose_name_plural = 'پیام‌ها'
+        ordering = ['timestamp']
+
+     def __str__(self):
+        return f"Message from {self.sender} in {self.Conversation.id}: {self.content[:30]}..."
+     
+     
